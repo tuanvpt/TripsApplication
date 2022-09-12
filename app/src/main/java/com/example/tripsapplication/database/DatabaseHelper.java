632c1cf0030019f45dba78c7bcbc6eb38268c09c
tuanvpt <tuanvpt@rikkeisoft.com>
2022-09-12 13:32:57 +0700

    #1 HomePage

--- app/src/main/java/com/example/tripsapplication/database/DatabaseHelper.java
+++ app/src/main/java/com/example/tripsapplication/database/DatabaseHelper.java
@@ -2,7 +2,7 @@ package com.example.tripsapplication.database;
 
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DatabaseHelper extends SQLiteOpenHelper {
+public class DatabaseHelper  {
 
 
 
--- app/src/main/java/com/example/tripsapplication/ui/HomeFragment.java
+++ app/src/main/java/com/example/tripsapplication/ui/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.tripsapplication.ui;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,98 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 
 import com.example.tripsapplication.R;
+import com.example.tripsapplication.base.BaseMVVMFragment;
+import com.example.tripsapplication.databinding.FragmentHomeBinding;
+import com.example.tripsapplication.utils.ViewUtils;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class HomeFragment extends Fragment {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class HomeFragment extends BaseMVVMFragment<FragmentHomeBinding, HomeViewModel> {
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
+    private boolean isAllFieldsChecked = false;
+    final Calendar myCalendar = Calendar.getInstance();
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+    @Override
+    protected Class<HomeViewModel> getViewModelClass() {
+        return HomeViewModel.class;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    protected FragmentHomeBinding getLayoutBinding() {
+        return FragmentHomeBinding.inflate(getLayoutInflater());
+    }
+
+    @Override
+    protected void initialize() {
+        handleDataOfTrips();
+        handleSubmitButton();
+    }
+
+    private void handleDataOfTrips() {
+        DatePickerDialog.OnDateSetListener date = (view, year, month, day) -> {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, month);
+            myCalendar.set(Calendar.DAY_OF_MONTH, day);
+            updateLabel();
+        };
+
+
+        getViewBinding().edtDataOfTrip.setClickable(true);
+        getViewBinding().edtDataOfTrip.setOnClickListener(view ->
+                new DatePickerDialog(requireContext(), date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show());
+    }
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
+        getViewBinding().edtDataOfTrip.setText(dateFormat.format(myCalendar.getTime()));
+    }
+
+    private void handleSubmitButton() {
+        getViewBinding().btnDatabase.setOnClickListener(view -> {
+            isAllFieldsChecked = CheckAllFields();
+            if (isAllFieldsChecked) {
+                showToast("True");
+            }
+        });
+
+    }
+
+    private boolean CheckAllFields() {
+        if (getViewBinding().edtName.length() == 0) {
+            getViewBinding().edtName.setError("This field is required");
+            return false;
         }
+
+        if (getViewBinding().edtDestination.length() == 0) {
+            getViewBinding().edtDestination.setError("This field is required");
+            return false;
+        }
+
+        if (getViewBinding().edtDataOfTrip.length() == 0) {
+            getViewBinding().edtDataOfTrip.setError("This field is required");
+            return false;
+        }
+
+
+        // after all validation return true.
+        return true;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    protected void registerViewEvent() {
+
+    }
+
+    @Override
+    protected void registerViewModelObs() {
+
     }
 }
